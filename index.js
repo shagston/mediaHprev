dca61f1a2de6e4daeab39465be68c599d4feaba1
D <47759704+shagston@users.noreply.github.com>
2019-03-06 17:29:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+const siteBanner = document.getElementById("site-banner");
+const newReleaseContent = document.getElementById("new-release-content");
+const topRatedContent = document.getElementById("top-rated-content");
+const popularContent = document.getElementById("popular-content");
+const apiKey = "b6b3aaa39e9b8519d35c42addbdba973";
+const keyword = "captain marvel";
+
+const url =
+  "https://api.themoviedb.org/3/search/movie?api_key=" +
+  apiKey +
+  "&query=" +
+  keyword;
+
+fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    let res = data.results;
+    console.log(res);
+
+    let mainTitle = document.createElement("h1");
+    let watchBtn = document.createElement("button");
+    let mainOverview = document.createElement("p");
+    let bgOverlay = document.createElement("div");
+
+    mainTitle.innerHTML = res[0].title;
+    watchBtn.innerHTML = "Watch now";
+    mainOverview.innerHTML = res[0].overview;
+    bgOverlay.classList.add("after");
+
+    mainTitle.className = "main-movie-title";
+    watchBtn.className = "watch-btn";
+    mainOverview.className = "main-overview";
+    bgOverlay.className = "after";
+
+    siteBanner.appendChild(mainTitle);
+    siteBanner.appendChild(mainOverview);
+    siteBanner.appendChild(watchBtn);
+    siteBanner.appendChild(bgOverlay);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const upcomingUrl =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=" +
+  apiKey +
+  "&language=ru&page=1";
+
+fetch(upcomingUrl)
+  .then(response => {
+    return response.json();
+  })
+  .then((data) => {
+    let res = data.results;
+    console.log(res);
+    for (var i = 0; i < 10; i++) {
+      let newReleaseMovie = document.createElement("div");
+      let newMovieTitle = document.createElement("h5");
+      let newMovieImg = document.createElement("img");
+
+      //Give elements a class name
+      newReleaseMovie.className = "movie";
+      newMovieTitle.className = "movie-title";
+      newMovieImg.className = "movie-img";
+
+      //fill in the elements
+      newMovieTitle.innerHTML = res[i].title;
+
+      newMovieImg.src = "https://image.tmdb.org/t/p/w200" + res[i].poster_path;
+
+      //append children to parent
+      newReleaseMovie.appendChild(newMovieImg);
+      newReleaseMovie.appendChild(newMovieTitle);
+      newReleaseContent.appendChild(newReleaseMovie);
+    }
+  })
+
+  .catch(err => {
+    console.log(err);
+  });
+
+const latestUrl =
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=" +
+  apiKey +
+  "&language=ru&page=1";
+
+fetch(latestUrl)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    let res = data.results;
+    console.log(res);
+    for (var i = 0; i < 10; i++) {
+      //Create elements
+      let topRatedMovie = document.createElement("div");
+      let topMovieTitle = document.createElement("h5");
+      let topMovieImg = document.createElement("img");
+
+      //Give elements a class name
+      topRatedMovie.className = "movie";
+      topMovieTitle.className = "movie-title";
+      topMovieImg.className = "movie-img";
+
+      //fill in the elements
+      topMovieTitle.innerHTML = res[i].title;
+
+      topMovieImg.src = "https://image.tmdb.org/t/p/w300" + res[i].backdrop_path;
+
+      //append children to parent
+      topRatedMovie.appendChild(topMovieImg);
+      topRatedMovie.appendChild(topMovieTitle);
+      topRatedContent.appendChild(topRatedMovie);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const popUrl =
+  "https://api.themoviedb.org/3/movie/popular?api_key=" +
+  apiKey +
+  "&language=ru&page=1";
+
+fetch(popUrl)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    let res = data.results;
+    console.log(res);
+    for (var i = 0; i < 10; i++) {
+      //Create elements
+      let popularMovie = document.createElement("div");
+      let popularMovieTitle = document.createElement("h5");
+      let popularMovieImg = document.createElement("img");
+
+      //give elements a classname
+      popularMovie.className = "movie";
+      popularMovieTitle.className = "movie-title";
+      popularMovieImg.className = "movie-img";
+
+      //fill in the elements
+      popularMovieTitle.innerHTML = res[i].title;
+      popularMovieImg.src =
+        "https://image.tmdb.org/t/p/w200" + res[i].poster_path;
+
+      //append popular section
+      popularMovie.appendChild(popularMovieImg);
+      popularMovie.appendChild(popularMovieTitle);
+      popularContent.appendChild(popularMovie);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
